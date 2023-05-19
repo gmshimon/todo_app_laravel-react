6541f967326738ae6a73201f64fa89e01718be50
gmshimon <simon.rosedale99@gmail.com>
2023-05-20 03:54:55 +0800

    fetch todo data from database

---
+++ app/Http/Controllers/Todo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class Todo extends Controller
+{
+    function getTodo(){
+       return DB:: select('select * from todo_list');
+    }
+}
