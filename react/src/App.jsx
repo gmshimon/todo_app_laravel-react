4fc4e15a19061904768364536c340be29ec6e878
gmshimon <simon.rosedale99@gmail.com>
2023-05-20 04:24:55 +0800

    basic layout

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,32 +1,41 @@
 // eslint-disable-next-line no-unused-vars
 import { useEffect, useState } from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+// eslint-disable-next-line no-unused-vars
+import { Button, Card } from "react-bootstrap";
+import Todo from "./Components/Todo";
 
 function App() {
+    const [todos, setTodos] = useState([]);
 
-  const [todos,setTodos] = useState([])
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/getTodo")
+            .then((res) => res.json())
+            .then((data) => setTodos(data));
+    }, []);
 
-  useEffect(()=>{
-    fetch('http://127.0.0.1:8000/getTodo')
-    .then(res=>res.json())
-    .then(data=>setTodos(data))
-  },[])
-
-  console.log(todos);
+    console.log(todos);
 
     return (
-        <>
-            <div>
-                <h1>Hello</h1>
-                {todos.map((todo) =>  (
-                    <div key={todo.id}>
-                        <h1>{todo.text}</h1>
-                    </div>
-                ))}
-            </div>
-        </>
+        <div className="app">
+          <div className="container">
+          <h1 className="text-center mb-4">Todo List</h1>
+            {todos.map((todo) => (
+                // eslint-disable-next-line react/jsx-key
+                <Card>
+                <Card.Body>
+                  <Todo key={todo.id}
+                  // index={index}
+                  todo={todo}
+                  // markTodo={markTodo}
+                  // removeTodo={removeTodo}
+                  />
+                </Card.Body>
+              </Card>
+            ))}
+        </div>
+        </div>
     );
 }
 
---
+++ react/src/Components/Todo.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+// eslint-disable-next-line react/prop-types
+const Todo = ({ todo, index, markTodo, removeTodo }) => {
+    console.log(todo);
+    return (
+        <div
+      className="todo"
+      
+    >
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+      <div>
+        <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
+        <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+      </div>
+    </div>
+    );
+};
+
+export default Todo;
