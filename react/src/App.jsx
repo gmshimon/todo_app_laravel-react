0eceeae1ae09448e0774eb65e3431e93b599ed4d
gmshimon <simon.rosedale99@gmail.com>
2023-05-20 04:03:20 +0800

    fetch and show data

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,32 +1,24 @@
 // eslint-disable-next-line no-unused-vars
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const todo_list = [
-    {
-        id: 1,
-        text: "Todo-1",
-        isDone: false,
-    },
-    {
-        id: 2,
-        text: "Todo-2",
-        isDone: false,
-    },
-    {
-        id: 3,
-        text: "Todo-3",
-        isDone: false,
-    },
-];
-
-console.log(todo_list);
 function App() {
+
+  const [todos,setTodos] = useState([])
+
+  useEffect(()=>{
+    fetch('http://127.0.0.1:8000/getTodo')
+    .then(res=>res.json())
+    .then(data=>setTodos(data))
+  },[])
+
+  console.log(todos);
+
     return (
         <>
             <div>
                 <h1>Hello</h1>
-                {todo_list.map((todo) =>  (
+                {todos.map((todo) =>  (
                     <div key={todo.id}>
                         <h1>{todo.text}</h1>
                     </div>
