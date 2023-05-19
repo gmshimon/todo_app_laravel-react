91780b5abbf45411eabed6ee1f04d709e5b7bcfc
gmshimon <simon.rosedale99@gmail.com>
2023-05-20 07:09:05 +0800

    post api created

--- app/Http/Controllers/Todo.php
+++ app/Http/Controllers/Todo.php
@@ -9,4 +9,8 @@ class Todo extends Controller
     function getTodo(){
        return DB:: select('select * from todo_list');
     }
+
+    function addTodo(Request $request){
+        return $request->input();
+    }
 }
